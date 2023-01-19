0af216f8ad6e2929d775be9c45c325a5a12cebc7
Mehdi Abdi <abdimehdi1321@gmail.com>
2023-01-19 18:19:29 +0330

    now airports endpoint works

--- ticket/src/presentation/routes/getAirports.js
+++ ticket/src/presentation/routes/getAirports.js
@@ -1,9 +1,34 @@
-// TODO: get a city in params and returns its airports in response.
+import db from "../../database/db.js";
+import APIError from "../../utils/api-error.js";
+import xor from "../../utils/xor.js";
+
 export const getAirports = {
   endpoint: "airports",
   method: "GET",
   private: false,
   handler: async (req, res) => {
-    res.status(200).json("get airports of a city");
+    let { city, country } = req.query;
+    let queryText,
+      queryResult,
+      airports,
+      values = [];
+
+    if (!xor(city, country))
+      throw new APIError("you should specify city, or country", 400);
+
+    queryText = "SELECT * FROM airport WHERE";
+    if (country) {
+      queryText += " country_name=$1";
+      values.push(country);
+    }
+    if (city) {
+      queryText += " city_name=$1";
+      values.push(city);
+    }
+
+    queryResult = await db.query(queryText, values);
+    airports = queryResult.rows;
+
+    res.status(200).json(airports);
   },
 };
