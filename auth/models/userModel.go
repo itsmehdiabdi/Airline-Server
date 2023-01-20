9a9f26f01753140488b4e3d3c3ee2b22f320a421
Mohammad Amin Lotfi <a.lotfi1201@gmail.com>
2023-01-20 16:22:16 +0330

    fix: update user json model

--- auth/models/userModel.go
+++ auth/models/userModel.go
@@ -1,11 +1,11 @@
 package models
 
 type UserAccount struct {
-	ID           int64  `gorm:"primary_key;auto_increment;not_null"`
-	Email        string `gorm:"unique;not null;default:null;uniqueIndex"`
-	PhoneNumber  string `gorm:"unique;not null;default:null;uniqueIndex"`
-	Male         bool   `gorm:"type:bool"`
-	FirstName    string
-	LastName     string
-	PasswordHash string
+	ID           int64  `json:"id" gorm:"primary_key;auto_increment;not_null"`
+	Email        string `json:"email" gorm:"unique;not null;default:null;uniqueIndex"`
+	PhoneNumber  string `json:"phoneNumber" gorm:"unique;not null;default:null;uniqueIndex"`
+	Male         bool   `json:"male" gorm:"type:bool"`
+	FirstName    string `json:"firstName"`
+	LastName     string `json:"lastName"`
+	PasswordHash string `json:"-"`
 }
