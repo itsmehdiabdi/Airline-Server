db2c0f3470cf3fba12380fd8eb0d85660a4438e1
Mehdi Abdi <abdimehdi1321@gmail.com>
2023-01-21 20:12:37 +0330

    ticket: add auth url to .env

--- ticket/src/utils/env.js
+++ ticket/src/utils/env.js
@@ -2,10 +2,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// TODO: add auth server url.
 let envVars = {
   DB_URL: `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   PORT: process.env.PORT,
+  AUTH_URL: process.env.AUTH_URL,
 };
 
 export default envVars;
